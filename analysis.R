a601a20e7243eabc5f9523589d8ff0286633b254
j <sylviana@uw.edu>
2019-02-07 17:05:02 -0800

    Build report

--- analysis.R
+++ analysis.R
@@ -6,21 +6,47 @@ library("tidyr")
 library("ggmap")
 
 # Load in your data
+evictions <- read.csv("data/Eviction_Notices.csv", stringsAsFactors = FALSE)
 
 # Compute some values of interest and store them in variables for the report
 
 # How many evictions were there?
+num_evictions <- nrow(evictions)
+num_features <- ncol(evictions)
 
 # Create a table (data frame) of evictions by zip code (sort descending)
+by_zip <- evictions %>%
+  group_by(Eviction.Notice.Source.Zipcode) %>%
+  count() %>%
+  arrange(-n) %>% #default colname n
+  ungroup() %>%
+  top_n(10, wt = n)
 
 # Create a plot of the number of evictions each month in the dataset
+by_month <- evictions %>%
+  mutate(date = as.Date(File.Date, format = "%m/%d/%y")) %>%
+  mutate(month = floor_date(date, unit = "month")) %>%
+  group_by(month) %>%
+  count()
 
 # Store plot in a variable
+plot_by_month <- ggplot(by_month) +
+  geom_line(aes(x = month, y = n), color = "blue") + 
+  labs(x = "Date", y = "Number of Evictions", title = "Evictions over time in SF")
 
 # Map evictions in 2017 
 
 # Format the lat/long variables, filter to 2017
+evictions_2017 <- evictions %>%
+  mutate(date = as.Date(File.Date, format = "%m/%d/%y")) %>%
+  filter(format(date, "%Y") == "2017") %>%
+  separate(Location, c("lat", "long"), ", ") %>%
+  mutate(
+    lat = as.numeric(gsub("\\(", "", lat)),
+    long = as.numeric(gsub("\\(", "", long))
+  )
 
 # Create a maptile background
+
 
 # Add a layer of points on top of the map tiles
