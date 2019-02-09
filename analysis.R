b32963269f3e28c03de4fed8cde16982cdb19305
j <sylviana@uw.edu>
2019-02-08 20:10:44 -0800

    review

--- analysis.R
+++ analysis.R
@@ -3,6 +3,7 @@ library("dplyr")
 library("ggplot2")
 library("lubridate")
 library("tidyr")
+devtools::install_github("dkahle/ggmap", ref = "tidyup", force = TRUE)
 library("ggmap")
 
 # Load in your data
@@ -17,36 +18,52 @@ num_features <- ncol(evictions)
 # Create a table (data frame) of evictions by zip code (sort descending)
 by_zip <- evictions %>%
   group_by(Eviction.Notice.Source.Zipcode) %>%
-  count() %>%
-  arrange(-n) %>% #default colname n
-  ungroup() %>%
-  top_n(10, wt = n)
+  count(sort = T) %>%
+  rename(zipcode = Eviction.Notice.Source.Zipcode, evictions = n) %>% 
+  ungroup() %>% 
+  top_n(10, wt = evictions)
 
 # Create a plot of the number of evictions each month in the dataset
 by_month <- evictions %>%
-  mutate(date = as.Date(File.Date, format = "%m/%d/%y")) %>%
-  mutate(month = floor_date(date, unit = "month")) %>%
-  group_by(month) %>%
+  group_by(
+    month = floor_date(as.Date(File.Date, format = "%m/%d/%y"), unit = "month")
+  ) %>%
   count()
 
 # Store plot in a variable
-plot_by_month <- ggplot(by_month) +
-  geom_line(aes(x = month, y = n), color = "blue") + 
-  labs(x = "Date", y = "Number of Evictions", title = "Evictions over time in SF")
+by_month_plot <- ggplot(by_month) +
+  geom_line(mapping = aes(x = month, y = n), size = .1, color = "blue") +
+  labs(
+    title = "Number of evictions each month in San Francisco", 
+    y = "Number of evictions", 
+    x = "Month"
+  )
 
 # Map evictions in 2017 
 
 # Format the lat/long variables, filter to 2017
-evictions_2017 <- evictions %>%
-  mutate(date = as.Date(File.Date, format = "%m/%d/%y")) %>%
+evictions_2017 <- evictions %>% 
+  mutate(date = as.Date(File.Date, format="%m/%d/%y")) %>% 
   filter(format(date, "%Y") == "2017") %>%
-  separate(Location, c("lat", "long"), ", ") %>%
+  separate(Location, c("lat", "long"), ", ") %>% # split the column at the comma
   mutate(
-    lat = as.numeric(gsub("\\(", "", lat)),
-    long = as.numeric(gsub("\\(", "", long))
-  )
+    lat = as.numeric(gsub("\\(", "", lat)), # remove starting parentheses
+    long = as.numeric(gsub("\\)", "", long)) # remove closing parentheses
+  ) 
 
 # Create a maptile background
-
+#base_plot <- qmplot(
+#  data = evictions_2017,        # name of the data frame
+#  x = long,                     # data feature for longitude
+#  y = lat,                      # data feature for latitude
+#  geom = "blank",               # don't display data points (yet)
+#  maptype = "toner-background", # map tiles to query
+#  darken = .7,                  # darken the map tiles
+#  legend = "topleft"            # location of legend on page
+#)
 
 # Add a layer of points on top of the map tiles
+#evictions_plot <- base_plot +
+#  geom_point(mapping = aes(x = long, y = lat), color = "red", alpha = .3) +
+#  labs(title = "Evictions in San Francisco, 2017") +
+#  theme(plot.margin = margin(.3, 0, 0, 0, "cm")) # adjust spacing around the map
